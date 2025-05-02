d6cbdc79d439b5d6f1ca67d35482ff9a69308f83
azurata <git@azurata.me>
2025-05-02 15:27:18 +0900

    test: デッドコードを削除して、コードがわかりやすくなるように整えた

--- test/prob1_test.cpp
+++ test/prob1_test.cpp
@@ -2,36 +2,38 @@
 #include "prob1.h"
 #include <stdio.h>
 
-void prob1_2before() {
-  printf("123\n");
-  printf("456\n");
-  printf("789\n");
-}
-
 TEST(Prob1Test, Prob1_1_Correct) {
   testing::internal::CaptureStdout();
+
   prob1_1();
+
   std::string output = testing::internal::GetCapturedStdout();
   EXPECT_STREQ(output.c_str(), "etrobo-fun\n");
 }
 
 TEST(Prob1Test, Prob1_2_Correct) {
   testing::internal::CaptureStdout();
+
   prob1_2();
+
   std::string output = testing::internal::GetCapturedStdout();
   EXPECT_STREQ(output.c_str(), "123\n456\n789\n");
 }
 
 TEST(Prob1Test, Prob1_3_Correct) {
   testing::internal::CaptureStdout();
+
   prob1_3();
+
   std::string output = testing::internal::GetCapturedStdout();
   EXPECT_STREQ(output.c_str(), "1 + 1 = 2\n2 + 3 = 5\n");
 }
 
 TEST(Prob1Test, Prob1_4_Correct) {
   testing::internal::CaptureStdout();
+
   prob1_4();
+
   std::string output = testing::internal::GetCapturedStdout();
   EXPECT_STREQ(output.c_str(), "1 + 2 + 3 = 6\n");
 }
